20489ce5e936ad4c2883967b3b68aedbb612d0d8
Lea <leasheilalabrador@gmail.com>
2025-10-02 01:23:48 +0800

    edit thankyouPage

--- components/ThankYouCard.jsx
+++ components/ThankYouCard.jsx
@@ -9,13 +9,8 @@ export default function ThankYouCard({ illustration, title, subtitle, message })
       <img src={illustration} alt="Thank You Illustration" className="w-80 h-60 md:w-40 md:h-50 mx-auto" />
       <h1 className="text-3xl md:text-3xl font-bold text-white">{title}</h1>
       <p className="text-xl md:text-base text-white">{subtitle}</p>
-      <p className="text-white text-lg mt-10">{message}</p>
-      <button
-        onClick={() => navigate("/")}
-        className="mt-4 bg-blue-900 text-white px-6 py-2 rounded-md font-semibold hover:bg-blue-800 transition"
-      >
-        HOME
-      </button>
+      <p className="text-white text-lg mt-5">{message}</p>
+      
     </div>
   );
 }
