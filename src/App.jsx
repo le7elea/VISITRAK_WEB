6b3890e36d10ed541084c2027f0aa39fe4e7f66f
Lea <leasheilalabrador@gmail.com>
2026-05-13 09:04:11 +0800

    as of May13

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import ExitScreen from "../pages/ExitScreen";
 import SuccessModal from "../pages/SuccessModal";
 import AccessGate from "../components/AccessGate";
 
+const EXIT_CONFIRM_MESSAGE = "Are you sure you want to leave VisiTrak?";
+
 function HomeRoute() {
   const location = useLocation();
   const navigate = useNavigate();
@@ -27,9 +29,52 @@ function HomeRoute() {
   return <VisiTrakForm />; 
 }
 
+function GlobalExitGuard() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = EXIT_CONFIRM_MESSAGE;
+      return EXIT_CONFIRM_MESSAGE;
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.history.pushState({ visiTrakExitGuard: true }, "");
+
+    const handlePopState = () => {
+      const shouldLeave = window.confirm(EXIT_CONFIRM_MESSAGE);
+
+      if (shouldLeave) {
+        window.removeEventListener("popstate", handlePopState);
+        window.history.back();
+        return;
+      }
+
+      window.history.pushState({ visiTrakExitGuard: true }, "");
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [location.pathname, location.search]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <Router>
+      <GlobalExitGuard />
       <AccessGate>
         <Routes>
           {/* Registration Form Page */}
