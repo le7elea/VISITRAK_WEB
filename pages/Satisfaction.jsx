e298b2485898827f6254edb59ae06f5180bede9f
Lea <leasheilalabrador@gmail.com>
2026-06-23 21:47:45 +0800

    satisfaction form update

--- pages/Satisfaction.jsx
+++ pages/Satisfaction.jsx
@@ -644,7 +644,7 @@ const Satisfaction = () => {
 
           <div
             ref={formCardRef}
-            className="bg-[#efefef] rounded-2xl p-4 sm:p-6 md:p-7 border border-white/80 shadow-xl"
+            className="bg-white rounded-2xl p-4 sm:p-6 md:p-7 border border-white/80 shadow-xl"
           >
             <FeedbackHeader />
 
